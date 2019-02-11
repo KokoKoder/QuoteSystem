2a40d9542e4ed290cc2186868211a261c79bc8b7
KokoKoder <nicolas.hoquet@gmail.com>
2019-02-11 13:39:03 +0200

    Update enter_item.blade.php

--- resources/views/enter_item.blade.php
+++ resources/views/enter_item.blade.php
@@ -28,7 +28,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	else{
 		$item_weight='0';
 	}
-	
+	if ($_POST["item_length"]!=""){
+	   $item_length=mysqli_real_escape_string($conn,$_POST["item_length"]);
+	}
+	else{
+	    $item_length='0';
+	}
+	if ($_POST["item_width"]!=""){
+	   $item_width=mysqli_real_escape_string($conn,$_POST["item_width"]);
+	}
+	else{
+	    $item_width='0';
+	}
+	if ($_POST["item_description"]!=""){
+	$item_description=mysqli_real_escape_string($conn,$_POST["item_description"]);
+	}
+	else{
+	    $item_description='';
+	}
+	if ($_POST["package_length"]!=""){
+	$package_length=mysqli_real_escape_string($conn,$_POST["package_length"]);
+	}
+	else{
+	    $package_length='0';
+	}
+	if ($_POST["package_width"]!=""){
+	    $package_width=mysqli_real_escape_string($conn,$_POST["package_width"]);
+	}
+	else{
+	    $package_width='0';
+	}
+	if ($_POST["package_height"]!=""){
+	$package_height=mysqli_real_escape_string($conn,$_POST["package_height"]);
+	}
+	else{
+	    $package_height='0';
+	}
+	if ($_POST["package_weight"]!=""){
+	$package_weight=mysqli_real_escape_string($conn,$_POST["package_weight"]);
+	}
+	else{
+	    $package_weight='0';
+	}
+	if ($_POST["item_per_pack"]!=""){
+	$item_per_pack=mysqli_real_escape_string($conn,$_POST["item_per_pack"]);
+	}
+	else{
+	    $item_per_pack='0';
+	}
 	$duplicate_check_sql = "SELECT item_name FROM items WHERE item_name = '$item_name'";
 	$check_results = mysqli_query($conn, $duplicate_check_sql);
 	if (mysqli_num_rows($check_results) > 0) {
@@ -36,17 +83,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	}
 
 	else{
-		$sql = "INSERT INTO items(item_name,supplier_sku,item_supplier_id,item_price,item_height,item_weight)
-		VALUES ('$item_name','$supplier_sku','$item_supplier_id','$item_price', '$item_height','$item_weight')";
+		$sql = "INSERT INTO items(item_name,supplier_sku,item_supplier_id,item_price,item_length,item_width,item_height,item_weight,item_description,package_length,package_width,package_height,package_weight,item_per_pack)
+		VALUES ('$item_name','$supplier_sku','$item_supplier_id','$item_price', '$item_length','$item_width','$item_height','$item_weight','$item_description','$package_length','$package_width','$package_height','$package_weight','$item_per_pack')";
 		if ($conn->query($sql) === TRUE) {
 			echo "New record created successfully in orders tab";
 		} else {
 			echo "Error: " . $sql . "<br>" . $conn->error;
 		}
-
-
+		
 		$conn->close();
-
+		
 		if (isset($_SESSION["order_id"])){
 		$_SESSION['product_name']=$item_name;
 		$url=route('add_item_to_order_form');
@@ -101,14 +147,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			 </div>
 		</div>
 		<div class="row">
-			<div class="input-field col s6">
-			  <input id="item_height" type="text" name="item_height" class="validate" >
-			  <label for="item_height">Size in m. Ex: 1.25</label>
+			<div class="input-field col s6 m3">
+			  <input id="item_width" type="text" name="item_width" class="validate" >
+			  <label for="item_width">Product width in mm Ex: 1250</label>
 			</div>
 
-			<div class="input-field col s6">
-			  <input id="item_weight" name="item_weight"  type="text" class="validate" >
-			  <label for="item_weight">Weight in kg. Ex: 0.5</label>
+			<div class="input-field col s6 m3">
+			  <input id="item_length" name="item_length"  type="text" class="validate" >
+			  <label for="item_length">Product length in mm</label>
+			</div>
+
+			<div class="input-field col s6 m3">
+			  <input id="item_height" type="text" name="item_height" class="validate" >
+			  <label for="item_height">Product height in mm</label>
+			</div>
+			<div class="input-field col s6 m3">
+			  <input id="item_weight" type="text" name="item_weight" class="validate" >
+			  <label for="item_weight">Product weight in kg - Ex: 2.5</label>
+			</div>
+		</div>
+		<div class="row">
+        	<div class="input-field col s12">
+          		<textarea id="item_description" class="materialize-textarea" name="item_description" class="validate"></textarea>
+          		<label for="item_description">Product description</label>
+        	</div>
+      	</div>
+		<div class="row">
+			<div class="input-field col s6 m3">
+			  <input id="package_width" type="text" name="package_width" class="validate" >
+			  <label for="package_width">Package width in mm</label>
+			</div>
+			<div class="input-field col s6 m3">
+			  <input id="package_length" name="package_length"  type="text" class="validate" >
+			  <label for="package_length">Package length</label>
+			</div>
+
+			<div class="input-field col s6 m3">
+			  <input id="package_height" type="text" name="package_height" class="validate" >
+			  <label for="package_height">Package height</label>
+			</div>
+			<div class="input-field col s6 m3">
+			  <input id="package_weight" name="package_weight"  type="text" class="validate" >
+			  <label for="package_weight">Package weight in kg - Ex: 2.5</label>
+			</div>
+		</div>
+		<div class="row">
+			<div class="input-field col s6 m3">
+			  <input id="item_per_pack" type="text" name="item_per_pack" class="validate" >
+			  <label for="item_per_pack">Item per package </label>
+			  <span class="helper-text">2 for 2 items per pack <br> 1/2 for 2 packs per item <br> 1/3 for item coming in 3 packs</span>
 			</div>
 		</div>
 		<button type="submit" class="btn">Enter Product</button>
