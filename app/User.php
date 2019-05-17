ce08c95cc1f0d8e1a45d01b8f3740d2cd6cb3f7b
KokoKoder <nicolas.hoquet@gmail.com>
2019-05-17 12:35:56 +0300

    isAdmin() function

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
