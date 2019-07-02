2cf3c7ba38bd3a2d421056d84485366ac8550d62
KokoKoder <nicolas.hoquet@gmail.com>
2019-07-02 16:38:36 +0300

    registration number added to edit customer

--- resources/views/edit_customer.blade.php
+++ resources/views/edit_customer.blade.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	        
 	    }
 	if (empty($err_duplicate_customer)){
-    	$sql_update="UPDATE customers SET customer_name='$customer_name', customer_address='$customer_address', customer_mail='$customer_mail',customer_phone='$customer_phone','registration_nb'=$registration_nb,vat_id='$vat_id' WHERE customer_id='$customer_id'";
+    	$sql_update="UPDATE customers SET customer_name='$customer_name', customer_address='$customer_address', customer_mail='$customer_mail',customer_phone='$customer_phone',registration_nb='$registration_nb',vat_id='$vat_id' WHERE customer_id='$customer_id'";
     	echo "POST to customer ID : ".$customer_id."address".$customer_address."mail".$customer_mail."phone".$customer_phone;
     	if ($conn->query($sql_update) === TRUE) {
         echo "Record updated successfully";
@@ -83,62 +83,59 @@ if (isset($_GET["customer_id"])){
 		</div>
 	</div>
     <div class="section">
-  <div class="row">
+  	<div class="row">
     <form autocomplete="off" method="POST" action="{{route('edit_customer')}}" class="col s12">
 
 	@csrf
-	  <div class="row">
-		<div class="col s12">
-	  
-	  	</div>
-	</div>
-	<div class="row">
-
-		
-		  <div class="input-field col s6">	
-			<i class="material-icons prefix">account_circle</i><input name="customer_name" id="customer_name" value="<?php if (isset($customer_id)){echo $customer_details[0];} ?>" >
-		  <span class="helper-text">Enter customer name</span>
-		  <span style="color:red"><?php if (!empty($err_duplicate_customer)){echo $err_duplicate_customer;}?></span>
-		  </div>
-		  <div class="input-field col s6">
-		  <i class="material-icons prefix">home</i>
-			<input id="address" type="text" name="customer_address" value="<?php if (isset($customer_id)){echo $customer_details[1];} ?>">
-			<span class="helper-text">Enter customer address</span>
-		  </div>
-
-	</div>
-	
-	<div class="row">
-		  <div class="input-field col s6">
-		  <i class="material-icons prefix">mail</i>
-			<input id="email" name="customer_mail" type="email" class="validate" value="<?php if (isset($customer_id)){echo $customer_details[2];} ?>">
-			<span class="helper-text" data-error="wrong" data-success="right">Enter customer email</span>
-		  </div>
-		  
-		  <div class="input-field col s6">
-		  <i class="material-icons prefix">phone</i>
-			<input id="phone" type="tel" name="customer_phone" class="validate" value="<?php if (isset($customer_id)){echo $customer_details[3];} ?>">
-			<span class="helper-text">Enter customer phone</span>
-		  </div>
-
-	</div>
-	<div class="row">
-		  <div class="input-field col s6">
-		  <i class="material-icons prefix">V</i>
-			<input id="vat_id" type="text" name="vat_id"  value="<?php if (isset($customer_id)){echo $customer_details[4];} ?>">
-			<span class="helper-text">Enter VAT ID</span>
-		  </div>
-		  <div class="input-field col s6">
-		  <i class="material-icons prefix">R</i>
-			<input id="registration_nb" type="text" name="registration_nb"  value="<?php if (isset($customer_id)){echo $customer_details[5];} ?>">
-			<span class="helper-text">Enter Registration number</span>
-		  </div>
-	</div>
+    	  <div class="row">
+    		<div class="col s12">
+    	  
+    	  	</div>
+		</div>
+    	<div class="row">
+    
+    		
+    		  <div class="input-field col s6">	
+    			<i class="material-icons prefix">account_circle</i><input name="customer_name" id="customer_name" value="<?php if (isset($customer_id)){echo $customer_details[0];} ?>" >
+    		  <span class="helper-text">Enter customer name</span>
+    		  <span style="color:red"><?php if (!empty($err_duplicate_customer)){echo $err_duplicate_customer;}?></span>
+    		  </div>
+    		  <div class="input-field col s6">
+    		  <i class="material-icons prefix">home</i>
+    			<input id="address" type="text" name="customer_address" value="<?php if (isset($customer_id)){echo $customer_details[1];} ?>">
+    			<span class="helper-text">Enter customer address</span>
+    		  </div>
+    
+    	</div>
+    	<div class="row">
+    		  <div class="input-field col s6">
+    		  <i class="material-icons prefix">mail</i>
+    			<input id="email" name="customer_mail" type="email" class="validate" value="<?php if (isset($customer_id)){echo $customer_details[2];} ?>">
+    			<span class="helper-text" data-error="wrong" data-success="right">Enter customer email</span>
+    		  </div>
+    		  
+    		  <div class="input-field col s6">
+    		  <i class="material-icons prefix">phone</i>
+    			<input id="phone" type="tel" name="customer_phone" class="validate" value="<?php if (isset($customer_id)){echo $customer_details[3];} ?>">
+    			<span class="helper-text">Enter customer phone</span>
+    		  </div>
+    	</div>
+    	<div class="row">
+    		  <div class="input-field col s6">
+    		  <i class="material-icons prefix">V</i>
+    			<input id="vat_id" type="text" name="vat_id"  value="<?php if (isset($customer_id)){echo $customer_details[4];} ?>">
+    			<span class="helper-text">Enter VAT ID</span>
+    		  </div>
+    		  <div class="input-field col s6">
+    		  <i class="material-icons prefix">R</i>
+    			<input id="registration_nb" type="text" name="registration_nb"  value="<?php if (isset($customer_id)){echo $customer_details[5];} ?>">
+    			<span class="helper-text">Enter Registration number</span>
+    		  </div>
+    	</div>
 	  <button class="waves-effect waves-light btn" type="submit" value="Submit">Edit customer details</button>
     </form>
   </div>
  </div>
-</div> 
 @endsection
 @push('scripts')
 	// Get list of existing Customers
