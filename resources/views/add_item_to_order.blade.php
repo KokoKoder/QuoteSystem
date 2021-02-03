2223860f7eaf17f3c7495942d9ef6399036255ad
KokoKoder <nicolas.hoquet@gmail.com>
2021-02-03 14:31:46 +0200

    debugging add item to order

--- resources/views/add_item_to_order.blade.php
+++ resources/views/add_item_to_order.blade.php
@@ -14,56 +14,56 @@ foreach($required as $field) {
 
 if ($error) {
 	$_SESSION["Err_missing_data"]="Name and quantity required";
-	$url=route('add_item_to_order_form');
+	$_SESSION["Err_missing_data"] = "Name and quantity required";
+	$url = route('add_item_to_order_form');
 	header("Location: ".$url);
-} else {
-  echo "Proceed...";
+	die;
 }
 
 
 #get the form value
-$product_name=mysqli_real_escape_string($conn,$_POST["product_name"]);
-$product_quantity=mysqli_real_escape_string($conn,$_POST["product_quantity"]);
+$product_name = mysqli_real_escape_string($conn,$_POST["product_name"]);
+$product_quantity = mysqli_real_escape_string($conn,$_POST["product_quantity"]);
 if (isset($_POST["schedule_date"])){
-	$Schedule_delivery_date=mysqli_real_escape_string($conn,$_POST["schedule_date"]);
+	$Schedule_delivery_date = mysqli_real_escape_string($conn,$_POST["schedule_date"]);
 }
 else{
 	$Schedule_delivery_date="";
-	
 }
 if (isset($_SESSION['product_name'])){unset($_SESSION['product_name']);}
-$order_id=mysqli_real_escape_string($conn,$_SESSION["order_id"]);
-$status_id="2";
+$order_id = mysqli_real_escape_string($conn,$_SESSION["order_id"]);
+$status_id = "2";
 
 #get item id
-$sql2 = "SELECT item_id,item_price FROM items WHERE item_name='$product_name'";
+$sql2 = "SELECT item_id,item_price FROM items WHERE item_name = '$product_name'";
 $result = mysqli_query($conn, $sql2);
 if (mysqli_num_rows($result) > 0) {
 	while($row = mysqli_fetch_assoc($result)) {
-		$item_id=$row['item_id'];
-		$item_price=$row['item_price'];
+		$item_id = $row['item_id'];
+		$item_price = $row['item_price'];
 		};
 }
 else{
-	$url=route('enter_item');
+	$url = route('enter_item');
 	header("Location: ".$url);
+	die;
 }
 #get standard delivery time if it was not set in the form and set the schedule delivery date
-if ($Schedule_delivery_date==""){
+if ($Schedule_delivery_date == ""){
 	#get the order date to calculate the schedule delivery date by adding the standard delivery time for the supplier
 	$order_date_sql="SELECT order_date  FROM orders_table  WHERE order_id='$order_id='";
 	$order_date_result = mysqli_query($conn, $order_date_sql);
 	if (mysqli_num_rows($order_date_result) > 0) {
 		while($row = mysqli_fetch_assoc($order_date_result)) {
-			$order_date=$row['order_date'];
-			};
+			$order_date = $row['order_date'];
+			}
 	}
 	#get the standard delivery time for the supplier 
-	$standard_delivery_time_sql="SELECT standard_delivery_time FROM suppliers JOIN items ON suppliers.supplier_id=items.item_supplier_id WHERE item_id='$item_id'";
+	$standard_delivery_time_sql = "SELECT standard_delivery_time FROM suppliers JOIN items ON suppliers.supplier_id=items.item_supplier_id WHERE item_id = '$item_id'";
 	$standard_delivery_time_result = mysqli_query($conn, $standard_delivery_time_sql);
 	if (mysqli_num_rows($standard_delivery_time_result) > 0) {
 		while($row = mysqli_fetch_assoc($standard_delivery_time_result)) {
-			$standard_delivery_time=$row['standard_delivery_time'];
+			$standard_delivery_time = $row['standard_delivery_time'];
 			};
 	}
 	#calculate the Schedule delivery date
@@ -79,12 +79,12 @@ if ($conn->query($sql) === TRUE) {
 	if (isset($_SESSION["order_edit"])){
 		echo  "This is a edit order session : ".$_SESSION["order_edit"];
 		echo "for the order : ".$_SESSION["order_id"];
-		$url=route('edit_order');
+		$url = route('edit_order');
 		header("Location: ".$url.'?order_id='.$_SESSION["order_id"]);
 	}
 	else{
 		echo "This is a new order session";
-		$url=route('add_item_to_order_form');
+		$url = route('add_item_to_order_form');
 		header("Location: ".$url);
 		exit;
 	}	
--- resources/views/edit_custom_order_item.blade.php
+++ resources/views/edit_custom_order_item.blade.php
@@ -5,12 +5,12 @@ session_start();
 include(app_path().'/includes/connect.php');
 
 #get the form value
-$id=mysqli_real_escape_string($conn,$_POST["id"]);
-$item_quantity=mysqli_real_escape_string($conn,$_POST["item_quantity"]);
-$Schedule_delivery_date=mysqli_real_escape_string($conn,$_POST["Schedule_delivery_date"]);
+$id = mysqli_real_escape_string($conn,$_POST["id"]);
+$item_quantity = mysqli_real_escape_string($conn,$_POST["item_quantity"]);
+$Schedule_delivery_date = mysqli_real_escape_string($conn,$_POST["Schedule_delivery_date"]);
 
 echo $Schedule_delivery_date;
-$item_status_id=mysqli_real_escape_string($conn,$_POST["item_status"]);
+$item_status_id = mysqli_real_escape_string($conn,$_POST["item_status"]);
 echo $id;
 $sql = "UPDATE order_custom_items SET item_quantity='$item_quantity',Schedule_delivery_date='$Schedule_delivery_date',status_id='$item_status_id' WHERE id='$id'";
 if ($conn->query($sql) === TRUE) {
@@ -21,17 +21,17 @@ if ($conn->query($sql) === TRUE) {
 }
 $conn->close();
 if (isset($_SESSION["order_id"]) AND !isset($_SESSION["order_edit"])){
-	$url=route('add_item_to_order_form');
+	$url = route('add_item_to_order_form');
 	header("Location: ".$url);
 exit;
 }
 elseif (isset($_SESSION["order_id"]) AND isset($_SESSION["order_edit"])){
-	$edit_order_url=route('edit_order');
+	$edit_order_url = route('edit_order');
 	header("Location: ".$edit_order_url."?order_id=".$_SESSION["order_id"]);
 	exit;
 }
 else{
-	$enter_item_url=route('enter_item');
+	$enter_item_url = route('enter_item');
 	header("Location: ".$enter_item_url);
 	exit;
 }
